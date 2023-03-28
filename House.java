3cbc9c7ffbfec04ddd17f24cc87e43b4dec24f9b
Your Name <you@example.com>
2023-03-28 01:16:08 -0400

    phase 1

--- House.java
+++ House.java
@@ -1,12 +1,61 @@
 /* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;  
+public class House extends Building {
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
+  // attributes
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+
+
+  /**
+   * 
+   * @param hasDiningRoom
+   */
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
+  }
+
+  // getters and setters
+
+  /**
+   * 
+   * @return
+   */
+  public boolean hasDiningRoom(){
+    return hasDiningRoom;
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public int nResidents(){
+    return residents.size();
+  }
+
+  public void moveIn(String name){
+    this.residents.add(name);
+  }
+
+  public String moveOut(String name){
+    for(int i = 0; i < residents.size(); i++){
+      if(residents.get(i).equals(name)){
+        String nm = name;
+        residents.remove(i);
+        return nm;
+      }
+    }
+    return name + " was not found so they weren't removed.";
+  }
+
+  public boolean isResident(String name){
+    return this.residents.contains(name);
   }
 
   public static void main(String[] args) {
-    new House();
+    new House("King House", "10 Elm Street", 4, true);
   }
 
 }
