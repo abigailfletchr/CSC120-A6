cc044352ab32c622e16944128544d953c19e12c4
Your Name <you@example.com>
2023-03-28 10:38:41 -0400

    part of phase 2

--- Library.java
+++ Library.java
@@ -1,10 +1,32 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
+/* This is a stub for the Library class */
+public class Library extends Building{
+
+  // attritubutes
+  private Hashtable<String, Boolean> collection;
+
+
+  // constructor
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      // System.out.println("You have built a library: 📖");
+      this.collection = new Hashtable<String, Boolean>();
     }
-  
+
+    public void addTitle(String title){
+      
+    }
+
+
+    public void removeTitle(String title){
+
+
+    } 
+
+
+
+
     public static void main(String[] args) {
       new Library();
     }
