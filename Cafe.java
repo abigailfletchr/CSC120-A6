d2e7cc8794f64c90faf2688644c4c81b90ee2b3e
Your Name <you@example.com>
2023-03-28 11:47:02 -0400

    phase 3

--- Cafe.java
+++ Cafe.java
@@ -1,10 +1,62 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building {
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
+
+    /**
+     * 
+     * @param name
+     * @param address
+     * @param nFloors
+     */
+    public Cafe(String name, String address, int nFloors) {
+        // System.out.println("You have built a cafe: ☕");
+        super(name, address, nFloors);
+        this.nCoffeeOunces = 60;
+        this.nSugarPackets = 35;
+        this.nCreams = 35;
+        this.nCups = 75;
     }
     
+    /**
+     * 
+     * @param size
+     * @param nSugarPackets
+     * @param nCreams
+     */
+    public void sellCoffee(int size, int nSugarPackets, int nCreams){
+        this.nCoffeeOunces = nCoffeeOunces - size;
+        this.nSugarPackets = nSugarPackets - nSugarPackets;
+        this.nCreams = nCreams - nCreams;
+        this.nCups = nCups -1;
+
+    }
+
+    /**
+     * 
+     * @param nCoffeeOunces
+     * @param nSugarPackets
+     * @param nCreams
+     * @param nCups
+     */
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
+        if(nCoffeeOunces < 10){
+            this.nCoffeeOunces = 60;
+        }
+        if(nSugarPackets < 2){
+            this.nSugarPackets = 35;
+        }
+        if(nCreams < 2){
+            this.nCreams = 35;
+        }
+        if(nCups < 10){
+            this.nCups = 75;
+        }
+    }
+
     public static void main(String[] args) {
         new Cafe();
     }
--- Library.java
+++ Library.java
@@ -8,17 +8,31 @@ public class Library extends Building{
 
 
   // constructor
+
+  /**
+   * 
+   * @param name
+   * @param address
+   * @param nFloors
+   */
     public Library(String name, String address, int nFloors) {
       super(name, address, nFloors);
       // System.out.println("You have built a library: 📖");
       this.collection = new Hashtable<String, Boolean>();
     }
 
+    /**
+     * 
+     * @param title
+     */
     public void addTitle(String title){
       this.collection.put(title, true); // have chai look 
     }
 
-
+    /**
+     * 
+     * @param title
+     */
     public void removeTitle(String title){
       for(int i = 0; i < collection.size(); i++){
         if(collection.get(i).equals(title)){ // have chai look
@@ -30,14 +44,26 @@ public class Library extends Building{
       System.out.println(title + " was not found and not removed.");
     }
     
+    /**
+     * 
+     */
     public void checkOut(String title){
       this.collection.replace(title, true, false); 
     }
 
+    /**
+     * 
+     * @param title
+     */
     public void returnBook(String title){
       this.collection.replace(title, false, true);
     }
 
+    /**
+     * 
+     * @param title
+     * @return
+     */
     public boolean containsTitle(String title){ // have chai look
       for(int i = 0; i < collection.size(); i++){
         if(collection.get(i).equals(title)){
@@ -48,6 +74,11 @@ public class Library extends Building{
       return false;
     }
 
+    /**
+     * 
+     * @param title
+     * @return
+     */
     public boolean isAvailable(String title){
       for(int i = 0; i < collection.size(); i++){
         if(collection.containsKey(title)){
@@ -61,6 +92,9 @@ public class Library extends Building{
     }
 
 
+    /**
+     * 
+     */
     public void printCollection(){
       System.out.println(collection.toString());
     }
