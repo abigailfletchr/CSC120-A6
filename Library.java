bfb0bfb9068fdf246bd68f0873e535e4449c8f8b
Your Name <you@example.com>
2023-03-28 11:19:52 -0400

    finished phase 2 ?? need help

--- Library.java
+++ Library.java
@@ -15,14 +15,57 @@ public class Library extends Building{
     }
 
     public void addTitle(String title){
-      
+      this.collection.put(title, true); // have chai look 
     }
 
 
     public void removeTitle(String title){
+      for(int i = 0; i < collection.size(); i++){
+        if(collection.get(i).equals(title)){ // have chai look
+          String tt = title;
+          collection.remove(i);
+          System.out.println(tt);
+        }
+      }
+      System.out.println(title + " was not found and not removed.");
+    }
+    
+    public void checkOut(String title){
+      this.collection.replace(title, true, false); 
+    }
+
+    public void returnBook(String title){
+      this.collection.replace(title, false, true);
+    }
+
+    public boolean containsTitle(String title){ // have chai look
+      for(int i = 0; i < collection.size(); i++){
+        if(collection.get(i).equals(title)){
+          return true;
+
+        }
+      }
+      return false;
+    }
+
+    public boolean isAvailable(String title){
+      for(int i = 0; i < collection.size(); i++){
+        if(collection.containsKey(title)){
+          if(collection.contains(true)){
+            return true;
+          }
+        }
+
+      }
+      return false;
+    }
 
 
-    } 
+    public void printCollection(){
+      System.out.println(collection.toString());
+    }
+
+    
 
 
 
